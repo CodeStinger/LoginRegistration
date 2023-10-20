d832e6c83870ae4fa6fee289584d2a5095e60fca
CodeStinger <vivekv.chavan.03@gmail.com>
2023-10-21 00:12:19 +0530

    registration page added

---
+++ Login/src/main/java/com/servlets/LoginServlet.java
@@ -0,0 +1,24 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String str = req.getParameter("userName");
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		//out.print(str);
+		out.print("<center><h1>Registration Successfull<h1><center>");
+		out.print("<center><p> Welcome "+str+" </p></center>");
+	}
+
+}
